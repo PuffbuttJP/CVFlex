860d51b55454e499962ab4cd24e039f2c630b3b1
PuffbuttJP <65969209+PuffbuttJP@users.noreply.github.com>
2022-05-17 19:51:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+function showProf (){
+    document.querySelector(`#academic`).style.display='none';
+    document.querySelector(`#personal`).style.display='none';
+}
+
+function showAcad (){}
+
+function showPers (){}
+
+function showAll(){}
